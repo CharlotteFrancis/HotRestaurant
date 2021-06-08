03dd95882babfdb306fa137d900fa795a6d7b725
bronsonsoda <83525261+bronsonsoda@users.noreply.github.com>
2021-06-08 13:22:14 -0700

    route dir

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,14 +2,14 @@ var path = require("path")
 
 module.exports = function(app)
 {
-  app.get("tables", function(req, res)
+  app.get("/tables", function(req, res)
   {
-    res.sendFile(path.join(__dirname, "../public/"))
+    res.sendFile(path.join(__dirname, "../public/tables.html"))
   })
 
   app.get"(/reserve", function(req, res) 
   {
-    res.sendFile(path.join(__dirname, "../public"))
+    res.sendFile(path.join(__dirname, "../public/reserve.html"))
   })
 }
 //dsdsd
