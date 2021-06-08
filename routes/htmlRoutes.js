b353af8ff44e13f19d9728ad3ac4234d1e258b88
Bronson Perez <bperez1081@gmail.com>
2021-06-08 13:11:43 -0700

    add html dir to routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,12 @@ module.exports = function(app)
 {
   app.get("tables", function(req, res)
   {
-    res.sendFile(path.join(__dirname, "../public/"))
+    res.sendFile(path.join(__dirname, "../public/tables.html"))
   })
 
   app.get"(/reserve", function(req, res) 
   {
-    res.sendFile(path.join(__dirname, "../public"))
+    res.sendFile(path.join(__dirname, "../public/reserve.html"))
   })
 }
 //dsdsd
