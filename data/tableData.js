2cc6fa6e4360370aa3affc61c59f7a727829d5d9
Bronson Perez <bperez1081@gmail.com>
2021-06-08 11:59:13 -0700

    routes updated

--- data/tableData.js
+++
@@ -1,8 +0,0 @@
-var arrayGuest = [
-  {
-    customerName: "Bronson"
-    customerEmail: "bronson@gmail.com"
-    customerID: "12345"
-    phoneNumber: "555-555-5555"
-  }
-]
--- data/waitingData.js
+++
@@ -1,8 +0,0 @@
-var arrayWaiting = [
-  {
-    customerName: "Who"
-    customerEmail: "who@yahoo.com"
-    phoneNumber: "123-456-7890"
-    custumerID: "54321"
-  }
-]
